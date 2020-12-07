b973c51f219c8d56440352a5719bad32a05ee3ea
hhaodu <hhaodu@tencent.com>
2020-12-07 15:35:21 +0800

    init

---
+++ seek/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <p>
+      这是一次声音的实验，我们要求您佩戴好耳机，跟随我们的脚步，一起进入一段轻松的旅程
+    </p>
+    <h3>点击屏幕开始吧</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.hello {
+  padding: 80px 30px 80px;
+}
+h3 {
+  margin-top: 100px;
+}
+</style>
