83b54d3658d071dc65577f7b6923e8806fc874f0
elizhai <elizhai@tencent.com>
2020-12-08 20:40:27 +0800

    Revert "语音模块"
    This reverts commit cec632629c0166ee8b7788326a34ea871321a2ce.

--- seek/src/views/Home.vue
+++ seek/src/views/Home.vue
@@ -1,29 +1,17 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App" />
-    <button @click="playAudio">播放</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
-import { playAudioByStep } from '../utils/playAudio.js';
 
 export default {
   name: "Home",
   components: {
     HelloWorld
-  },
-  data () {
-    return {
-      setp: 0
-    }
-  },
-  methods: {
-    playAudio() {
-      playAudioByStep(this.setp);
-    }
   }
 };
 </script>
